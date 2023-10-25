ee3493c0e8a55e6a7ccee18de73b9e489a29fa93
fastleopard9372 <fastleopard9372@gmail.com>
2023-10-25 16:56:20 +0900

    sds

--- application/views/faq.php
+++ application/views/faq.php
@@ -1,151 +1,23 @@
+
 <div id="secondary-content-wrapper" style="margin-top:50px;">
-<div class="container clearfix">
-    <h1 style="margin-left:50px;">FAQ</h1>
+    <div class="container clearfix">
+			<h1 style="margin-left:50px;">FAQ</h1>
+    </div>
 </div>
-</div>
-<div id="content-wrapper" style="margin:0px 50px;">
+<div id="content-wrapper" style="background:white;">
     <div class="container">
-        <div id="columns">
-            <div class="columns-inner clearfix">
-                <div id="content-column" style="margin-top:-80px;">
-                    <div class="content-inner">
-                        <section id="main-content">
-                            <div id="content">
-                                <div id="block-system-main" class="block block-system no-title odd first last block-count-10 block-region-content block-main">
-                                    <div class="view view-faq view-id-faq view-display-id-page">
-                                        <div class="view-content">
-                                            <div class="views-row views-row-1 views-row-odd views-row-first">
-                                                <div class="ds-1col node node-faq node-promoted article odd node-full view-mode-full clearfix">
-                                                    <div class="field field-name-title">
-                                                        <h3>What is Alex’s Lemonade Stand Foundation?</h3>
-                                                    </div>
-                                                    <div class="field field-name-body">
-                                                        <p>Alex's Lemonade Stand Foundation (ALSF) emerged from the front yard lemonade stand of 4-year-old Alexandra “Alex” Scott, who was fighting cancer and wanted to raise money to find cures for all children with cancer. Her spirit and determination inspired others to support her cause, and when she passed away at the age of 8, she had raised $1 million. Since then, the Foundation bearing her name has evolved into a national fundraising movement. Today, ALSF is one of the leading funders of pediatric cancer research in the U.S. and Canada having raised more than $250 million so far, funding over 1,000 research projects and providing programs to families affected by childhood cancer.</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="views-row views-row-2 views-row-even">
-                                                <div class="ds-1col node node-faq node-promoted article even node-full view-mode-full clearfix">
-                                                    <div class="field field-name-title">
-                                                        <h3>Who was Alex?</h3>
-                                                    </div>
-                                                    <div class="field field-name-body">
-                                                        <p>
-                                                            Alex is the inspiration behind Alex’s Lemonade Stand Foundation. <a href="https://www.alexslemonade.org/about/meet-alex">Alexandra “Alex” Scott</a>
-                                                            (1996-2004) was diagnosed with neuroblastoma, a rare form of childhood cancer, before her first birthday. At the age of 4, Alex announced that she wanted to hold a lemonade stand to raise money to help find a cure for all children with cancer.
-                                                        </p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="views-row views-row-3 views-row-odd">
-                                                <div class="ds-1col node node-faq node-promoted article odd node-full view-mode-full clearfix">
-                                                    <div class="field field-name-title">
-                                                        <h3>Where does the money go?</h3>
-                                                    </div>
-                                                    <div class="field field-name-body">
-                                                        <p>
-                                                            All funds raised go directly to Alex’s Lemonade Stand Foundation for Childhood Cancer Research, a registered 501(c)3 charity. Careful distribution of our grants allows doctors to bring the latest and most promising life-saving treatments to seriously ill children. These grants provide hope for kids fighting cancer. Funds also support programs to help families affected by childhood cancer. Visit our <a href="/grants">Where the Money Goes</a>
-                                                            section to learn more about how your donation helps.
-                                                        </p>
-                                                        <div style="margin: 10px 20px 0 10px;"></div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="views-row views-row-4 views-row-even">
-                                                <div class="ds-1col node node-faq node-promoted article even node-full view-mode-full clearfix">
-                                                    <div class="field field-name-title">
-                                                        <h3>What is Alex’s Lemonade Stand Foundation’s mission?</h3>
-                                                    </div>
-                                                    <div class="field field-name-body">
-                                                        <p>To change the lives of children with cancer through funding impactful research, raising awareness, supporting families, and empowering everyone to help cure childhood cancer.</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="views-row views-row-5 views-row-odd">
-                                                <div class="ds-1col node node-faq node-promoted article odd node-full view-mode-full clearfix">
-                                                    <div class="field field-name-title">
-                                                        <h3>What is Alex’s Lemonade Stand Foundation’s vision?</h3>
-                                                    </div>
-                                                    <div class="field field-name-body">
-                                                        <p>We share the vision of our founder and creator, Alexandra “Alex” Scott – a cure for all children with cancer.</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="views-row views-row-6 views-row-even">
-                                                <div class="ds-1col node node-faq node-promoted article even node-full view-mode-full clearfix">
-                                                    <div class="field field-name-title">
-                                                        <h3>How does Alex’s Lemonade Stand Foundation raise funds?</h3>
-                                                    </div>
-                                                    <div class="field field-name-body">
-                                                        <p>Our donations come from several sources, including: devoted volunteers who host fundraisers; partnerships with businesses and corporations; and individual donations made directly to our Foundation.</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="views-row views-row-7 views-row-odd">
-                                                <div class="ds-1col node node-faq node-promoted article odd node-full view-mode-full clearfix">
-                                                    <div class="field field-name-title">
-                                                        <h3>Can anyone hold a lemonade stand?</h3>
-                                                    </div>
-                                                    <div class="field field-name-body">
-                                                        <p>
-                                                            Yes! We believe that everyone can make a difference, and we encourage everyone to get involved. Lemonade stands have been organized by families and businesses and we love to work with people of all ages. You should <a href="/campaign/stands-and-events/plan-lemonade-stand">register your stand through our website</a>
-                                                            beforehand so one of our stand coaches can help you along and send you materials to support your fundraising efforts. By registering online, you have the option for supporters who can't visit your stand or event in-person to donate online.
-                                                        </p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="views-row views-row-8 views-row-even">
-                                                <div class="ds-1col node node-faq node-promoted article even node-full view-mode-full clearfix">
-                                                    <div class="field field-name-title">
-                                                        <h3>Does Alex’s Lemonade Stand Foundation support one hospital or one type of research?</h3>
-                                                    </div>
-                                                    <div class="field field-name-body">
-                                                        <p>Our mission is to find a cure for all childhood cancers and that allows us to work with researchers from near and far. Put simply, we believe in funding the best and most promising research into all types of childhood cancers and have funded projects at nearly 150 leading hospitals and institutions across the United States and Canada.  Researchers from all not-for-profit institutions in North America are encouraged to apply.</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="views-row views-row-9 views-row-odd">
-                                                <div class="ds-1col node node-faq node-promoted article odd node-full view-mode-full clearfix">
-                                                    <div class="field field-name-title">
-                                                        <h3>How much money has Alex’s Lemonade Stand Foundation raised?</h3>
-                                                    </div>
-                                                    <div class="field field-name-body">
-                                                        <p>
-                                                            We have raised more than $250 million for childhood cancer, and we have funded over 1,000 research projects at nearly 150 institutions. Additionally, we have important programs to help families struggling with childhood cancer, such as our <a href="/travel-for-care">Travel For Care</a>
-                                                            program and <a href="/campaign/supersibs-sibling-support-childhood-cancer-families">SuperSibs</a>
-                                                            .
-                                                        </p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="views-row views-row-10 views-row-even views-row-last">
-                                                <div class="ds-1col node node-faq node-promoted article even node-full view-mode-full clearfix">
-                                                    <div class="field field-name-title">
-                                                        <h3>How much of the money raised goes to programs and research versus administrative/fundraising costs?</h3>
-                                                    </div>
-                                                    <div class="field field-name-body">
-                                                        <p>Alex was committed to making every donation count, and the Foundation bearing her name has carried on that vision of giving as much as possible to research and programs. Thanks to the lemonade stand hosts, our generous sponsors and donors, we have been able to keep our administrative and fundraising expenses at a minimum. In 2022, our fundraising and administrative expenses combined were approximately 12%.</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="views-row views-row-10 views-row-even views-row-last">
-                                                <div class="ds-1col node node-faq node-promoted article even node-full view-mode-full clearfix" style="height:250px;">
-                                                    <div class="field field-name-title">
-                                                        <h3>How much of the money raised goes to programs and research versus administrative/fundraising costs?</h3>
-                                                    </div>
-                                                    <div class="field field-name-body">
-                                                        <p>Alex was committed to making every donation count, and the Foundation bearing her name has carried on that vision of giving as much as possible to research and programs. Thanks to the lemonade stand hosts, our generous sponsors and donors, we have been able to keep our administrative and fundraising expenses at a minimum. In 2022, our fundraising and administrative expenses combined were approximately 12%.</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </section>
-                    </div>
-                </div>
-            </div>
+        <div class="columns school-container" style="background: url(<?= ASSETS_URL ?>sites/default/files/bg-schools.jpg);">
+        	<div class="school-modal">
+            <h3>
+								Frequently Asked Questionss 
+            </h3>
+						<p>
+                What is Booktopia?						
+            </p>
+						<p style="text-align:center;">
+							<a href="#" class="button" >Contact Us Today To Get Started!</a>
+						</p>
+        	</div>
         </div>
     </div>
 </div>
