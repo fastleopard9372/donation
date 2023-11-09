57be10481cca9a6146c2b456101bd62b0767d37d
jacobux30 <fastleopard9372@gmail.com>
2023-11-10 02:51:08 +0900

    update

--- application/config/config.php
+++ application/config/config.php
@@ -518,6 +518,6 @@ $config['rewrite_short_tags'] = FALSE;
  * | as well as specifying whole subnets. Here are a few examples:
  * |
  * | Comma-separated:	'10.0.1.200,192.168.5.0/24'
- * | Array:		array('10.0.1.200', '192.168.5.0/24')
+ * | Array:array('10.0.1.200', '192.168.5.0/24')
  */
 $config['proxy_ips'] = '';
